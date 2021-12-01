202f48d9fb5ff4e08830049c695a9384c6b2ed0e
Woodstock <victorhe97@gmail.com>
2021-12-01 13:32:36 -0500

    Added query selectors for the items and instructions legends

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Game from './scripts/game.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   const startGame = document.querySelector('#start-btn');
-  
+
   startGame.addEventListener('click', (e) => {
     e.preventDefault();
     hideGameOverMessage();
@@ -23,6 +23,9 @@ window.addEventListener('DOMContentLoaded', () => {
     let game = new Game();
     game.kickOff();
   });
+
+  const instructions =
+
 });
 
 function removeMenu() {
@@ -33,4 +36,24 @@ function removeMenu() {
 function hideGameOverMessage() {
   let hideMessage = document.getElementById('game-end');
   hideMessage.classList.add('hidden');
+}
+
+function revealInstructions() {
+  let instructionsLegend = document.getElementById("instructions-container");
+  instructionsLegend.classList.remove('hidden');
+}
+
+function hideInstructions() {
+  let instructionsLegend = document.getElementById("instructions-container");
+  instructionsLegend.classList.add('hidden');
+}
+
+function revealItems() {
+  let itemsLegend = document.getElementById("items-container");
+  itemsLegend.classList.remove('hidden');
+}
+
+function hideItems() {
+  let itemsLegend = document.getElementById("items-container");
+  itemsLegend.classList.add('hidden');
 }
