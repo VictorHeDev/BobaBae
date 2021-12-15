45e64a55c014d6b377fc6e6f9df64ffbd333c247
Woodstock <victorhe97@gmail.com>
2021-12-15 17:29:10 -0500

    Changed links to open on a new tab on click

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -1,4 +1,3 @@
-
 // In this file all we want to do is retrieve the attributes of the canvas and clear it upon each call to our game loop (in another file)
 // Might want to edit this later so we have this class do more things such as creating the canvas and hiding it
 // or even rendering another canvas in order to display a menu for starting the game, pausing, or game over
@@ -16,10 +15,10 @@ export default class Canvas {
   // }
 
   constructor() {
-    this.canvas = document.getElementById("game-canvas");
+    this.canvas = document.getElementById('game-canvas');
     this.canvas.width = 800;
     this.canvas.height = 600;
-    this.ctx = this.canvas.getContext("2d");
+    this.ctx = this.canvas.getContext('2d');
   }
 
   clearCanvas() {
@@ -27,9 +26,7 @@ export default class Canvas {
   }
 
   removeCanvas() {
-    let gameContainer = document.getElementsByClassName("game-container")
+    let gameContainer = document.getElementsByClassName('game-container');
     gameContainer.removeChild(this.canvas);
   }
-
-
 }
